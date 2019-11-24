a224b534dde07cab26b347c70ec4ebdb4b6756b3
stanley.traub@gmail.com <stanley.traub@gmail.com>
2019-11-24 14:14:31 -0800

    Finished problem

--- zeroMatrix.js
+++ zeroMatrix.js
@@ -2,7 +2,41 @@
 // its entire row and column are set to O.
 
 function zeroMatrix(matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+           if (matrix[i][j] === 0) matrix[i][j] = 'X';
+            
+        }
+    }
+    
 
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === 'X') {
+                // handle rows 
+                for (let column = 0; column < matrix[i].length; column++) {
+                    if (matrix[i][column] !== 'X') {
+                        matrix[i][column] = 0;
+                    }
+                    
+                }
+
+                // handle columns
+                for (let row = 0; row < matrix.length; row++) {
+                    if (matrix[row][j] !== 'X') {
+                        matrix[row][j] = 0;
+                    }
+
+                }
+
+                matrix[i][j] = 0;
+            }
+            
+        }
+        
+    }
+
+    return matrix;
 }
 
-console.log(zeroMatrix([[1, 2, 3], [0, 2, 1], [1, 3, 0]])) // => [[1,2,3],[0,0,0],[0,0,0]]
+console.log(zeroMatrix([[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]])); 
